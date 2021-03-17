fe636a977ab7ce659366f14ee203340fa9f91e14
peter1118 <songpetersd@gmail.com>
2021-03-17 23:02:48 +0900

    Home 컴포넌트 수정중

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,24 @@ import React from 'react'
 import './Home.css'
 
 function Home() {
-
-    return(
+    return (
         <div className="Home">
-            <h1>Hi, I'm Peter, !</h1>                    
+            <div className="Home-left">
+                <div className="Home-left-main">Hi, I'm Peter
+                    <div className="Home-left-main-sub">
+        프로답게 가치를 제공하고 싶은.<br />
+        기술자로 살고 싶은. <br />
+        사람을 키우고 싶은. </div>
+                </div>
+                <div className="Home-left-sub" id='home-about'>About me ></div>
+            </div>
+            <div className="Home-right">
+                <div className="Home-right-top">
+                    Home right top img
+                </div>
+            </div>
         </div>
-    ) 
+    )
 }
 
 export default Home
