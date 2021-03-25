8eafcf41dd157034d1ee9b50638561e1fcfea84d
peter1118 <songpetersd@gmail.com>
2021-03-26 00:37:36 +0900

    ttt

--- src/Work.js
+++ src/Work.js
@@ -16,7 +16,6 @@ function Work() {
                     {data.map((row) => (
                         <WorkCard key={row.alias} work={row.alias} />
                     ))}
-                    sdfsdfsf
                 </div>
             </div>
         </div>
