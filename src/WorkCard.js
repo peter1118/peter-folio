63d49aa27e5859f6a93a27afc55213ab22cc816d
peter1118 <songpetersd@gmail.com>
2021-03-26 00:26:55 +0900

    test

--- src/WorkCard.js
+++ src/WorkCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './WorkCard.css'
 
 function WorkCard({ work }) {
-    return <div className="WorkListCard-wrapper">work card of {work} !!</div>
+    return <div className="WorkListCard-wrapper">work card of {work} !@</div>
 }
 
 export default WorkCard
