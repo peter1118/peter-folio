77ab70bed4e85b9ad3e05294b553968f0a3f6b22
peter1118 <songpetersd@gmail.com>
2021-04-03 02:32:41 +0900

    work dialog 제작

--- src/WorkCardMore.js
+++ src/WorkCardMore.js
@@ -12,6 +12,8 @@ import IconButton from '@material-ui/core/IconButton'
 import Button from '@material-ui/core/Button'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LanguageIcon from '@material-ui/icons/Language'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
 
 function WorkCardMore({ idx, open, onClose }) {
     return (
@@ -19,9 +21,51 @@ function WorkCardMore({ idx, open, onClose }) {
             <Dialog open={open} onClose={onClose}>
                 <DialogTitle>{data[idx]['title']}</DialogTitle>
                 <DialogContent dividers={true}>
-                    <DialogContentText tabIndex={-1}>
-                        contents...
-                    </DialogContentText>
+                    <Typography component="div">
+                        <Box
+                            fontSize={18}
+                            letterSpacing={3}
+                            fontWeight="fontWeightBold"
+                            fontFamily="monospace"
+                            color="text.disabled"
+                            m={1}
+                        >
+                            개요
+                        </Box>
+                        <Box textAlign="left">
+                            {data[idx]['more']['header'][0]}
+                        </Box>
+                        <Box
+                            fontSize={18}
+                            letterSpacing={3}
+                            fontWeight="fontWeightBold"
+                            fontFamily="monospace"
+                            color="text.disabled"
+                            m={1}
+                        >
+                            상세 설명
+                        </Box>
+                        {data[idx]['more']['detail'].map((para, index) => (
+                            <Box key={index} textAlign="center">
+                                {para}
+                            </Box>
+                        ))}
+                        <Box
+                            fontSize={18}
+                            letterSpacing={3}
+                            fontWeight="fontWeightBold"
+                            fontFamily="monospace"
+                            color="text.disabled"
+                            m={1}
+                        >
+                            맡은 역할
+                        </Box>
+                        <ul className="WorkCardMore-role">
+                            {data[idx]['more']['role'].map((para, index) => (
+                                <li key={index}>{para}</li>
+                            ))}
+                        </ul>
+                    </Typography>
                 </DialogContent>
                 <DialogActions>
                     <IconButton color="primary">
