8d431e22bdbefdbe987448a30c2e3cefa2b6c048
peter1118 <songpetersd@gmail.com>
2021-04-11 21:14:12 +0900

    git 주소 추가

--- src/WorkData.js
+++ src/WorkData.js
@@ -6,7 +6,7 @@ const rows = [
     createData(
         '개인 포트폴리오 페이지',
         ['React, firebase', '2021, by my self'],
-        ['', '']
+        ['https://github.com/peter1118/peter-folio', '']
     ),
     createData(
         '다중 사용자간 공유 원격 데스크톱 서비스',
